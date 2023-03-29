f73033b094d3ed7c7aaeef7456c5a2103d513ab5
zhaojuntao.123 <zhaojuntao.123@bytedance.com>
2023-03-29 17:38:21 +0800

    integrated torch_int

--- setup.py
+++ setup.py
@@ -15,6 +15,25 @@ setup(
                              '-lculibos', '-lcudart', '-lcudart_static',
                              '-lrt', '-lpthread', '-ldl', '-L/usr/lib/x86_64-linux-gnu/'],
         ),
+        cpp_extension.CUDAExtension(
+            name='lptorch.torch_int._CUDA',
+            sources=[
+                'lptorch/torch_int/kernels/linear.cu',
+                'lptorch/torch_int/kernels/bmm.cu',
+                'lptorch/torch_int/kernels/fused.cu',
+                'lptorch/torch_int/kernels/bindings.cpp',
+            ],
+            include_dirs=[
+                'lptorch/torch_int/kernels/include',
+                'lptorch/torch_int/cutlass/include',
+                'lptorch/torch_int/cutlass/util/include',
+            ],
+            extra_link_args=['-lcublas_static', '-lcublasLt_static',
+                             '-lculibos', '-lcudart', '-lcudart_static',
+                             '-lrt', '-lpthread', '-ldl', '-L/usr/lib/x86_64-linux-gnu/'],
+            extra_compile_args={'cxx': ['-std=c++14', '-O3'],
+                                'nvcc': ['-O3', '-std=c++14', '-U__CUDA_NO_HALF_OPERATORS__', '-U__CUDA_NO_HALF_CONVERSIONS__', '-U__CUDA_NO_HALF2_OPERATORS__']},
+        ),
     ],
     cmdclass={
         'build_ext': cpp_extension.BuildExtension.with_options(use_ninja=False)
