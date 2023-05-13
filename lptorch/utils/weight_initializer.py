280277d247e0b204d9f55af7fd02525d7a0be13a
SpringWave1 <534239589@qq.com>
2023-05-13 15:52:04 +0800

    add some extra support

---
+++ lptorch/utils/weight_initializer.py
@@ -0,0 +1,28 @@
+import torch
+def init_weight_bias_with_rand(module):
+    if hasattr(module, 'weight'):
+        weight_shape = module.weight.shape
+        w_dtype = module.weight.dtype
+        if w_dtype == torch.float32 or torch.float16:
+            torch.nn.init.uniform_(module.weight)
+        else:
+            # int8
+            if w_dtype == torch.int8:
+                torch.randint(-127, 127, weight_shape, dtype=w_dtype, device=module.weight.device, out=module.weight)
+            else:
+                raise NotImplementedError
+    
+    if hasattr(module, 'bias'):
+        # first check if it is none
+        if module.bias is None:
+            return
+        bias_shape = module.bias.shape
+        b_dtype = module.bias.dtype
+        if b_dtype == torch.float32 or torch.float16:
+            torch.nn.init.uniform_(module.bias)
+        else:
+            # int8
+            if b_dtype == torch.int8:
+                torch.randint(-127, 127, bias_shape, dtype=b_dtype, device=module.bias.device, out=module.bias)
+            else:
+                raise NotImplementedError
