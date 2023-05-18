fa4629dd16af64cedb68567f6bae4d206ae6586e
SpringWave1 <534239589@qq.com>
2023-05-18 14:41:44 +0800

    use csv

--- lptorch/utils/__init__.py
+++ lptorch/utils/__init__.py
@@ -1,4 +1,4 @@
-from .get_device import get_capability, is_tensorcore_int8_available, is_tensorcore_int8_available_offline
+from .get_device import get_capability, is_tensorcore_int8_available, is_tensorcore_int8_available_offline, query_cc
 from .tensor_status import uniform_dtype
 from . import perf_utils
 from .module_status import get_model_size_cuda
